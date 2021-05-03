416e8ae1e4378450f51c29fb59538767ff77c497
kevith bastidas <kevithchi@unicauca.edu.co>
2021-05-03 00:02:29 -0500

    State

---
+++ State-OrderManagement/src/main/java/co/unicauca/ordermanagement/domain/states/PayedStated.java
@@ -0,0 +1,45 @@
+
+package co.unicauca.ordermanagement.domain.states;
+
+import co.unicauca.ordermanagement.domain.Order;
+
+/**
+ * Estado Pagado
+ * 
+ * @author Kevith Bastidas
+ */
+public class PayedStated extends OrderState{
+
+    /**
+     * constructor
+     * 
+     * @param order 
+     */
+    public PayedStated(Order order) {
+        super(order);
+    }
+    
+    /**
+     * Descripción del estado
+     * 
+     * @return estado
+     */
+    @Override
+    public String getStateDescription() {
+        return "Pagada";
+    }
+    
+    /**
+     * Metodo que enlaza la secuencia segun las reglas de negocio
+     * 
+     * @param parcelNumber
+     * @return 
+     */
+    @Override
+    public OrderState orderSendOut(String parcelNumber) {
+        return new SendState(getOrder());
+    }
+}
+
+
+
